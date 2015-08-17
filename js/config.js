4c48bc1f08dd2e42689b5439d2529a10c0ca538c
EpicXGamer <zachary.colone@yahoo.com>
2015-08-17 10:47:23 -0400

    thisismyoutubeloadingscreen

---
+++ js/config.js
@@ -0,0 +1,112 @@
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "ZX Zone";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = true;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/4K_Computer_Tech_Loading_Data_Animation_Background.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"",
+	"",
+	""
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{youtube: "KPAS1hqnm4g", name: "Take Back New York"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 45;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Enjoy the server",
+"Join our Steam Group ZX ZONE for upcomming events like deathrun!",
+"Swearing is definently not allowed so dont think about it"
+"Don't spawnkill... thats very important.. if they pull out a weapon then you can... this doesn't include toolguns or physguns"
+"EpicGamer241 is the owner... if someone says that their the owner then report to the administrators or me"
+"wan't to know more rules? type !motd in the chat"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
